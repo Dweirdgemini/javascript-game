cb73c10e288bed722cc0254da618b2ccbfd9c7b2
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-03-26 23:59:43 +0100

    hehehe

--- RPS.js
+++ RPS.js
@@ -1,10 +1,17 @@
+	// function choice button selector
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultOutputDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 
+	//display of result for clicking buttons
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
 	userChoiceDisplay.innerHTML = userChoice
+	generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+	const randomNumber = Math.random() * possibleChoices.length
+}
