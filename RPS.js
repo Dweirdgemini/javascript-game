1fbda1500a5d3db16fdb9cc3d2ee48528a801078
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-03-26 23:53:06 +0100

    master

--- RPS.js
+++ RPS.js
@@ -1,4 +1,10 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const resultOutput = document.getElementById('result')
-const possibleChoice = document.querySelectorAll('button')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultOutputDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+    userChoice = e.target.id
+	userChoiceDisplay.innerHTML = userChoice
+}))
