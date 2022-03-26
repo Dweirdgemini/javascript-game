3892fe05b472230b64554077d081664d93f3178e
Joshua Nwachuku <nwachukujosh2000@gmail.com>
2022-03-26 23:43:20 +0100

    new

---
+++ RPS.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById('computer-choice')
+const userChoice = document.getElementById('user-choice')
+const resultOutput = document.getElementById('result')
+const possibleChoice = document.querySelectorAll('button')
